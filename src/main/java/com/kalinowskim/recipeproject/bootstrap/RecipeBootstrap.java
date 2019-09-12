2a10e7f93cc254e9a8bf69ddb36c5fb2d7bee0cc
kalinowskim <kalinowskimateusz94@gmail.com>
2019-09-12 19:52:43 +0200

    Refactored with Lombok

--- src/main/java/com/kalinowskim/recipeproject/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/kalinowskim/recipeproject/bootstrap/RecipeBootstrap.java
@@ -4,6 +4,7 @@ import com.kalinowskim.recipeproject.domain.*;
 import com.kalinowskim.recipeproject.repositories.CategoryRepository;
 import com.kalinowskim.recipeproject.repositories.RecipeRepository;
 import com.kalinowskim.recipeproject.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -29,6 +31,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        log.debug("Loading Bootstrap Data...");
         recipeRepository.saveAll(getRecipes());
     }
 
